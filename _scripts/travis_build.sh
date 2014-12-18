c69b18a6921771d229e608ebbc16c8acf87c7e15
Jason Heppler <jason.heppler@gmail.com>
2014-12-18 14:20:34 -0800

    Adding test post for Travis CI

--- _scripts/travis_build.sh
+++ _scripts/travis_build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'master' ]] ; then
-  bundle exec rake stage
-elif [[ $TRAVIS_BRANCH == 'live' ]] ; then
-  bundle exec rake publish
-else
-  echo 'Invalid branch. You can only deploy from master and live.'
-  exit 1
-fi
+set -e # halt script on error
+
+bundle exec jekyll build
+bundle exec htmlproof ./_site
+
+#if [[ $TRAVIS_BRANCH == 'master' ]] ; then
+#  bundle exec rake stage
+#elif [[ $TRAVIS_BRANCH == 'live' ]] ; then
+#  bundle exec rake publish
+#else
+#  echo 'Invalid branch. You can only deploy from master and live.'
+#  exit 1
+#fi
