2582632cbd15ae1c851ef06f43f1da9976b90a8b
Jason Heppler <jason.heppler@gmail.com>
2015-02-10 12:44:17 -0800

    adding webhook

--- webhook.php
+++ webhook.php
@@ -1,16 +1,37 @@
-<?
-
-/*
-  Webhook for deploying Github updates to AFS
-*/
+<?php
+/**
+ * Simple PHP Git deploy script, intended to run with Jekyll. Automatically deploy
+ * a Jekyll site when changes to Github are committed.
+ *
+ * @version 0.1
+ * @author Jason Heppler (jason@jasonheppler.org)
+ * @link http://jasonheppler.org
+ *
+ * @license GNU General Public LIcense v3.0 - license.txt
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-$output = shell_exec('cd /afs/ir/users/j/h/jheppler/web/cesta && git pull');
-$build = shell_exec('cd ~/jheppler/web/cesta && jekyll build > dev/null&')
+/**
+ * Configure
+ */
 
-echo $output;
-echo $build;
+$LOCAL_ROOT       = "/afs/ir/users/j/h/jheppler/WWW/cesta";
+$REMOTE_REPO      = "https://github.com/hepplerj/ockham.git";
+
+/**
+ * Deployment
+ */
+
+echo shell_exec("cd {$LOCAL_ROOT} && git pull && jekyll build")
 
 ?>
